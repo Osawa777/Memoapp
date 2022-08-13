b7fecae29355e74215b8900c606e808ae43a7013
jun <j.osawa777@gmail.com>
2022-08-13 10:51:58 +0900

    Install @expo/vector-icon

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -21,7 +21,7 @@ export default function MemoDetailScreen() {
           本文ようなので使い方を間違えると不自然に見えることもありますので要注意。
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 160, buttom: 'auto' }}>+</CircleButton>
+      <CircleButton style={{ top: 160, buttom: 'auto' }} name="edit-2" />
     </View>
   );
 }
